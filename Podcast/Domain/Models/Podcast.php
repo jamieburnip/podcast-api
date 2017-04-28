bd84471431a1285336352cbe604374deced001e5
Jamie Burnip <jamieburnip@gmail.com>
2017-04-28 02:23:19 +0100

    Add file attribute to podcast

--- Podcast/Domain/Models/Podcast.php
+++ Podcast/Domain/Models/Podcast.php
@@ -3,8 +3,21 @@
 namespace Podcast\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Podcast extends Model
 {
-    //
+    /** @var string */
+    protected $table = 'podcast';
+
+    /**
+     * @return Collection
+     */
+    public function getFilesAttribute(): Collection
+    {
+        return collect([
+            'mp3' => $this->file_mp3,
+            'ogg' => $this->file_ogg,
+        ]);
+    }
 }
